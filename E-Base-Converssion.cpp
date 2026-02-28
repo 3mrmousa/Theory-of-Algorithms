3d7a3b1af8f8bc029e77bcf02e64c599d96bfd90
Amr Mousa <amrmoda123@gmail.com>
2026-02-28 20:38:13 +0200

    q5 commited

---
+++ E-Base-Converssion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void toBinary(int n)
+{
+    if (n == 0) return;
+    toBinary(n / 2);
+    cout << n % 2;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        toBinary(n);
+        cout << endl;
+    }
+}
