ab716fb7556038eba11c68b3efc0a8df34ac88af
Amr Mousa <amrmoda123@gmail.com>
2026-02-28 22:24:38 +0200

    q8 commited

---
+++ H-Inverted-Pyramid.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int input;
+
+void print(int num)
+{
+  
+  if(num == 0){
+    return;
+  }
+  
+
+  
+  for(int i = 0; i < input - num; i++){
+    cout<<" ";
+  }
+  for(int i = 0; i < num*2 - 1; i++){
+    cout<<"*";
+  }
+  
+  cout<<endl;
+  
+  
+  print(num - 1);
+}
+
+int main()
+{
+    int num;
+    cin>>num;
+    input = num;
+    print(num);
+}
