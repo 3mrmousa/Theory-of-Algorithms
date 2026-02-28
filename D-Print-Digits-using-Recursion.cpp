ec059eafe224d1a74fb2ba4413315866136a30ce
Amr Mousa <amrmoda123@gmail.com>
2026-02-28 20:13:30 +0200

    q4 commited

---
+++ D-Print-Digits-using-Recursion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void printDigits(int n)
+{
+    if (n < 10)
+    {
+        cout << n << " ";
+        return;
+    }
+
+    printDigits(n / 10);
+    cout << n % 10 << " ";
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (n == 0)
+            cout << 0 << " ";
+        else
+            printDigits(n);
+
+        cout << endl;
+    }
+}
